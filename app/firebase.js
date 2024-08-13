c7118a726fa70fed81d7a7e5c575b130c0e9f50e
annguyen32 <annnguyen32@gmail.com>
2024-08-13 14:13:01 -0500

    inital pages

---
+++ app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFi6eC2HXLRwNTUp6i8__u3CRuBVGYqQ4",
+  authDomain: "flashcard-saas-901ef.firebaseapp.com",
+  projectId: "flashcard-saas-901ef",
+  storageBucket: "flashcard-saas-901ef.appspot.com",
+  messagingSenderId: "434396865225",
+  appId: "1:434396865225:web:a109b3283a9422a97a2318",
+  measurementId: "G-C9PRDCBWSL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  AppBar,
+  Toolbar,
+  Button,
+} from "@mui/material";
+import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Page() {
+  <div> Hello </div>;
+  return <SignIn />;
+}
